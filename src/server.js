10a5b99b665eea4255d68c1fb107195041ed61d5
ogiogii <riswandiyhogi@gmail.com>
2026-04-25 17:35:09 +0700

    perbaikan

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
-import app from './app.js';
+import createServer from './Infrastructures/http/createServer.js';
+import container from './Infrastructures/container.js';
 import config from './Commons/config.js';
 
-const PORT = config.app.port || 5000;
+const start = async () => {
+  const server = await createServer(container);
+  const PORT = config.app.port || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server Forum API sedang berjalan di port ${PORT}`);
-  console.log(`Dokumentasi tersedia di http://localhost:${PORT}/api-docs`);
-});
+  server.listen(PORT, () => {
+    console.log(`Server Forum API sedang berjalan di port ${PORT}`);
+    console.log(`Dokumentasi tersedia di http://localhost:${PORT}/api-docs`);
+  });
+};
+
+start();
